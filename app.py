7ca86635e017e2d74efe5d4674cf626eaf5178d7
Wojtek <breeze.dnb@gmail.com>
2021-03-13 17:27:55 +0100

    implicit wait for elements, adding exceptions

--- app.py
+++ app.py
@@ -1,18 +1,15 @@
 from selenium import webdriver
 from datetime import datetime
-from time import sleep
 
 
 class PlaylistScrapper():
 
-	driver = None
-	current_date = None
-
 
 	def __init__(self):
 		self.driver = webdriver.Firefox()
+		self.driver.implicitly_wait(10)
 		self.driver.get("http://radioluz.pwr.edu.pl/")
-		self.current_date = datetime.now().strftime('%d-%m-%Y')
+		self.current_date = datetime.now().strftime('%Y-%m-%d')
 		self.main()
 
 
@@ -21,13 +18,19 @@ class PlaylistScrapper():
 		songs = []
 		path = "/html/body/div[2]/div/section[3]/div/ul/"
 
-		for i in range(1, 6):
-			time = self.driver.find_element_by_xpath(f"{path}li[{i}]/time").text
-			title = self.driver.find_element_by_xpath(f"{path}li[{i}]/div/p[1]").text
-			artist = self.driver.find_element_by_xpath(f"{path}li[{i}]/div/p[2]").text
-			songs.append(f'{time}  {artist} - {title}')
+		try:
+			for i in range(1, 6):
+				time = self.driver.find_element_by_xpath(f"{path}li[{i}]/time").text
+				title = self.driver.find_element_by_xpath(f"{path}li[{i}]/div/p[1]").text
+				artist = self.driver.find_element_by_xpath(f"{path}li[{i}]/div/p[2]").text
+				songs.append(f'{time}  {artist} - {title}')
+			return songs
 
-		return songs
+		except Exception as e:
+			raise e
+
+		finally:
+			self.driver.quit()
 
 
 	# returns a list of already saved songs
@@ -38,18 +41,21 @@ class PlaylistScrapper():
 		except:
 			return []
 
+
 	def write_songs_to_file(self, prev, songs):
-		with open(f'np_luz_{self.current_date}.txt', mode='a') as file:
-			for item in songs:
-				if item not in prev:
-					file.write(item+"\n")
+		try:
+			with open(f'np_luz_{self.current_date}.txt', mode='a') as file:
+				for item in songs:
+					if item not in prev:
+						file.write(item+"\n")
+		except Exception as e:
+			raise e
 
 
 	def main(self):
 		songs = self.get_songs()
 		prev = self.get_saved()
 		self.write_songs_to_file(prev, songs)
-		self.driver.quit()
 
 
 if __name__ == "__main__":
